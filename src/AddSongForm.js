9f09ef339d6b364b2e8d209758c073c6871ea40e
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2023-11-24 14:36:33 -0800

    colors go brrr

--- src/AddSongForm.js
+++ src/AddSongForm.js
@@ -1,17 +1,37 @@
 import React from "react";
-import {Button, Layout, Input, Space} from "antd";
-import { Header } from "antd/es/layout/layout";
+import {Button, Layout, Input, Space, ConfigProvider,theme} from "antd";
+import { Content } from "antd/es/layout/layout";
 
 function enter(){
     console.out("this")
 }
 
 function AddSongForm(){
+    
     return(
-        <Header>
+        <ConfigProvider
+    theme={{
+      token: {
+        // Seed Token
+        colorPrimary: '#7458e2',
+        // Alias Token
+        colorBgContainer: '#fbf3ff',
+        algorithm: theme.compactAlgorithm,
+      },
+    }}>
+        <Content>
+            <Space.Compact style={{width:'100%'}}>
+            <Input placeholder="Song Title" />
             <Button ghost>Enter</Button>
-            <Button ghost>new button</Button>
-        </Header>
+            </Space.Compact>
+        </Content>
+        <Content>
+            <Space.Compact style={{width:'100%'}}>
+            <Input placeholder="Lead Composer" />
+            <Button ghost>Enter</Button>
+            </Space.Compact>
+        </Content>
+        </ConfigProvider>
     )
 }
 export default AddSongForm
