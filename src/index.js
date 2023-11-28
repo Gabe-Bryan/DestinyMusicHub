c677b17eb8aec85edd33b2ce7fe584ee3888f18e
Gabe-Bryan <gabriel.bryan544@gmail.com>
2023-11-27 17:11:16 -0800

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import './style.css'
+import './style.css';
+import App from './App';
 import AddSongForm from './AddSongForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
