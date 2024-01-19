8c09330dd5abb78aadf8dc22dd69b3a120173df1
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2024-01-19 14:10:13 -0800

    figuring out ul

--- src/components/CompleteSoundtrackDisplay.js
+++ src/components/CompleteSoundtrackDisplay.js
@@ -4,13 +4,13 @@ const songList="hi my name is jeff"
 
 function CompleteDisplay({SongList}){
     const [expanded, setExpanded] = useState(false);
-
+    const [cdSize, setcdSize] = useState(['200', '110']);
 
 const expand= () => {
     setExpanded(!expanded);
 }
     return(
-        <div className="Complete-Song-Outer-Div">
+        <div className="Complete-Song-Outer-Div${expanded}?">
             <script>
             <ul>
                 <li onClick={expand}></li>
