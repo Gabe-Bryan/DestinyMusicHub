5dc24acdad1061989d8b382815b6aea0960dc1bd
alan8r <alan8r@gmail.com>
2024-02-11 23:30:38 -0800

    added more test tracks so the SoundtrackBanners render at max-height

--- src/pages/OfficialMusic.js
+++ src/pages/OfficialMusic.js
@@ -15,7 +15,50 @@ let testData = [
         composer: 'anonymous',
         length: '2:32',
     },
+    {
+        title: 'song 1',
+        track: 1,
+        composer: 'anonymous',
+        length: '1:58',
+    },
+    {
+        title: 'song 2',
+        track: 2,
+        composer: 'anonymous',
+        length: '2:32',
+    },
+    {
+        title: 'song 1',
+        track: 1,
+        composer: 'anonymous',
+        length: '1:58',
+    },
+    {
+        title: 'song 2',
+        track: 2,
+        composer: 'anonymous',
+        length: '2:32',
+    },
+    {
+        title: 'song 1',
+        track: 1,
+        composer: 'anonymous',
+        length: '1:58',
+    },
+    {
+        title: 'song 2',
+        track: 2,
+        composer: 'anonymous',
+        length: '2:32',
+    },
 ];
+
+let containerStyle = {
+    display: "block",
+    border: "solid 0px #f00",
+    textAlign: "center",
+    marginBottom: "5rem",
+}
 
 let colStyle = {
     display: "inline-block",
@@ -24,10 +67,8 @@ let colStyle = {
     verticalAlign: "top",
 };
 
-let containerStyle = {
-    display: "block",
-    border: "solid 0px #f00",
-    textAlign: "center",
+let innerColStyle = {
+    margin: "2rem 0rem",
 }
 
 export function OfficialMusic({ twoColumn = true }) {
@@ -40,20 +81,44 @@ export function OfficialMusic({ twoColumn = true }) {
                 <h2>Official Soundtracks / Released Music:</h2>
                 <div style={containerStyle}>
                     <div id="col1" style={colStyle}>
-                        <SoundtrackBanner expanded>
-                            <Playlist playlistData={testData} />
-                        </SoundtrackBanner>
-                    </div>
-                    {twoColumn && <>
-                        <div id="col2" style={colStyle}>
-                            <SoundtrackBanner expanded>
+                        <div style={innerColStyle}>
+                            <SoundtrackBanner>
                                 <Playlist playlistData={testData} />
                             </SoundtrackBanner>
                         </div>
+                        <div style={innerColStyle}>
+                            <SoundtrackBanner>
+                                <Playlist playlistData={testData} />
+                            </SoundtrackBanner>
+                        </div>
+                        <div style={innerColStyle}>
+                            <SoundtrackBanner>
+                                <Playlist playlistData={testData} />
+                            </SoundtrackBanner>
+                        </div>
+                    </div>
+                    {twoColumn && <>
+                        <div id="col2" style={colStyle}>
+                            <div style={innerColStyle}>
+                                <SoundtrackBanner>
+                                    <Playlist playlistData={testData} />
+                                </SoundtrackBanner>
+                            </div>
+                            <div style={innerColStyle}>
+                                <SoundtrackBanner>
+                                    <Playlist playlistData={testData} />
+                                </SoundtrackBanner>
+                            </div>
+                            <div style={innerColStyle}>
+                                <SoundtrackBanner>
+                                    <Playlist playlistData={testData} />
+                                </SoundtrackBanner>
+                            </div>
+                        </div>
                     </>}
                 </div>
-            </body>
-        </div>
+            </body >
+        </div >
     );
 
 }
