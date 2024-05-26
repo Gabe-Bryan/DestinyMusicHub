ebcc9d5cc5b13fb114ba24617aec0e62ee86850b
Gabe-Bryan <gabriel.bryan544@gmail.com>
2024-05-26 12:29:05 -0700

    Updates

--- src/AutoScalingText.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AutoScalingText ({children}) {
-    const test = () =>{
-        console.log("hi");
-    };
-    
-    return <div onchange={test}>{children}</div>
-}
